e73d239e511c1778a1218a458de3cb10b8f3ade8
LiunaShan <LiunaShan@users.noreply.github.com>
2026-08-04 18:31:30 +0000

    modify about me

--- app/components/Aboutme.tsx
+++ app/components/Aboutme.tsx
@@ -15,6 +15,34 @@ export default function Aboutme() {
     <br />
     For four years, I worked on enterprise systems.
   </p>
+  <p className={styles.title}>Career</p>
+
+<div className={styles.timeline}>
+
+  <div className={styles.item}>
+    <p className={styles.year}>2022 - 2026</p>
+
+    <div className={styles.content}>
+      <h3>Software Engineer</h3>
+      <p>Developed enterprise systems.</p>
+    </div>
+  </div>
+
+  <div className={styles.item}>
+    <p className={styles.year}>2026</p>
+
+    <div className={styles.content}>
+      <h3>Moved to Estonia</h3>
+      <p>Started my Working Holiday.</p>
+    </div>
+  </div>
+
+</div>
+  <p className={styles.title}>Strongth</p>
+
+  <p className={styles.text}>
+   problem solving
+  </p>
 
 </section>
  )
