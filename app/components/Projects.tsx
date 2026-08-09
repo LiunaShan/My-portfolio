ccb3664fcb5b3f9f5cbdff4bbe8b962c68d49ac9
LiunaShan <LiunaShan@users.noreply.github.com>
2026-08-09 07:56:49 +0000

    modify about me and projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import styles from "./Projects.module.css";
 
 type Project = {
-  icon: string;
+  image: string;
   title: string;
   description: string;
   details: string;
